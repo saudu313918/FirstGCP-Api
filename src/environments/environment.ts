e0f530a7aacd1325948cc6ee39c2aeb2b0fd5667
saudu313918 <saudagarshinde1@gmail.com>
2021-09-13 14:32:09 +0530

    first comm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  webapiurl: (window as any)["envconfig"]["apiurl"] || "default"                   
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
