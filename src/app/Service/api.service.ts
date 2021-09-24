33b6817eb751dc29c1dcaa6b2c43998ea231bf3d
saudu313918 <saudagarshinde1@gmail.com>
2021-09-24 09:30:34 +0530

    24/09/2021-first-commit-of the-day

--- src/app/Service/api.service.ts
+++ src/app/Service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { map } from "rxjs/operators";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,36 +13,40 @@ export class ApiService {
  //  $baseUrl = "localhost:8080/api/v1/";
   // API List
   // Post Data Of The Product
+   headers= new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*');
+  
 
   postProduct(value : any){
-    return this.http.post<any>('http://localhost:8080/posts', value)
+    return this.http.post<any>('https://8080-cf41a445-1761-453c-971a-db1dc491e529.cs-asia-southeast1-ajrg.cloudshell.dev/aliens', value)
     .pipe(map((res:any)=>{
       return res;
-    }))
+    },{ 'headers': this.headers }))
   }
 
   // Get Product Details from backend
   getProduct(){
-    return this.http.get<any>('http://localhost:8080/posts')
+    return this.http.get<any>('https://8080-cf41a445-1761-453c-971a-db1dc491e529.cs-asia-southeast1-ajrg.cloudshell.dev/aliens')
     .pipe(map((res : any)=>{
       return res;
-    }))
+    },{ 'headers': this.headers }))
   }
 
   //Edit the product details By ID
   updateProduct(value : any, id:number){
-    return this.http.put<any>('http://localhost:8080/posts/' +  id,value)
+    return this.http.put<any>('https://8080-cf41a445-1761-453c-971a-db1dc491e529.cs-asia-southeast1-ajrg.cloudshell.dev/aliens/' +  id,value)
     .pipe(map((res : any)=>{
       return res;
-    }))
+    },{ 'headers': this.headers }))
   }
 
   // Delete the product by ID
   deleteProduct(id : number){
-    return this.http.delete<any>('http://localhost:8080/posts/' + id)
+    return this.http.delete<any>('https://8080-cf41a445-1761-453c-971a-db1dc491e529.cs-asia-southeast1-ajrg.cloudshell.dev/aliens/' + id)
     .pipe(map((res : any)=>{
       return res;
-    }))
+    },{ 'headers': this.headers }))
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,   
-  webapiurl: (window as any)["envconfig"]["apiurl"] || "default"              
+  // webapiurl: (window as any)["envconfig"]["apiurl"] || "default"              
 };
 /*
  * For easier debugging in development mode, you can import the following file
